674a58d6e4ab06b23fe15133a6df2531bbcc86e1
fisa712 <101712610+fisa712@users.noreply.github.com>
2023-03-22 22:18:54 +0500

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,76 +1,63 @@
-from flask import Flask, jsonify, request, render_template
-import requests
-import json
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+
+
+# In[6]:
+
+
+from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
+import pickle
+from sklearn.preprocessing import StandardScaler
 
-# Fetch the Bitcoin mining data from Blockchain.com
-def fetch_data():
-    url = 'https://api.blockchain.info/charts/hash-rate?timespan=1week&format=json'
-    response = requests.get(url)
-    data = json.loads(response.text)
-    df = pd.DataFrame(data['values'], columns=['time', 'hash_rate'])
-    df['time'] = pd.to_datetime(df['time'], unit='s')
-    df.set_index('time', inplace=True)
-    return df
-
-# Preprocess the data
-def preprocess_data(df):
-    df['hash_rate_diff'] = df['hash_rate'].diff()
-    df.dropna(inplace=True)
-    return df
-
-# Train a linear regression model on the data
-def train_model(df):
-    X = df['hash_rate_diff'].values.reshape(-1, 1)
-    y = df['hash_rate'].values.reshape(-1, 1)
-    model = LinearRegression().fit(X, y)
-    return model
-
-# Create the Flask app
 app = Flask(__name__)
 
-# Define the routes
-@app.route('/')
-def live_dashboard():
-    # Fetch the data and preprocess it
-    df = fetch_data()
-    df = preprocess_data(df)
-    
-    # Train the model
-    model = train_model(df)
-    
-    # Predict the next hash rate
-    next_hash_rate = model.predict([[df['hash_rate_diff'].iloc[-1]]])[0][0]
-    
-    # Create the plot
-    fig = make_subplots(rows=1, cols=1)
-    fig.add_trace(go.Scatter(x=df.index, y=df['hash_rate'], name='Hash Rate'))
-    fig.add_trace(go.Scatter(x=[df.index[-1], df.index[-1]+pd.Timedelta(hours=1)], y=[df['hash_rate'].iloc[-1], next_hash_rate], name='Next Hash Rate', mode='lines'))
-    fig.update_layout(title='Bitcoin Hash Rate', xaxis_title='Time', yaxis_title='Hash Rate (TH/s)', showlegend=True)
-    
-    # Render the dashboard
-    return render_template('dashboard.html', plot=fig.to_html())
+# Load the trained machine learning model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
+# Load the Bitcoin live stream data into a pandas DataFrame
+df = pd.read_csv('accurate.csv')
+
+# Preprocess the data
+df['time'] = pd.to_datetime(df['time'])
+scaler = StandardScaler()
+df['hash_rate'] = scaler.fit_transform(df['hash_rate'].values.reshape(-1,1))
+# new addition of route 
+@app.route('/')
+def home():
+    return "Welcome to the Bitcoin Live Stream Dashboard"
+# Define the dashboard route
+@app.route('/dashboard')
+def dashboard():
+    # Get the live data and predictions from the machine learning model
+    live_data = df.tail(1).values[0]
+    prediction = model.predict(live_data.reshape(1,-1))[0]
+    # Render the HTML template containing the live data and predictions
+    return render_template('dashboard.html', live_data=live_data, prediction=prediction)
+
+# Define the predict route
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Fetch the data and preprocess it
-    df = fetch_data()
-    df = preprocess_data(df)
-    
-    # Train the model
-    model = train_model(df)
-    
-    # Get the input data from the user
-    input_data = request.form['input_data']
-    
-    # Make the prediction using the trained model
-    prediction = model.predict([[float(input_data)]])[0][0]
-    
-    # Return the prediction as a JSON response
-    return jsonify({'prediction': prediction})
+    # Get the live data from the POST request
+    live_data = request.form['live_data']
+    live_data = scaler.transform([float(live_data)])
+    # Get the accuracy of the machine learning model for the live data
+    accuracy = model.score(live_data.reshape(1,-1))
+    # Return the accuracy as a string
+    return str(accuracy)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
